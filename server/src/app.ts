c52e48a5bd68539d92d911911530c1e98a442bf6
Organique0 <grabnar.luka@gmail.com>
2023-05-15 23:31:49 +0200

    showing the decks on the page

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,13 @@
 import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
 import Deck from "./models/Deck";
+import cors from "cors";
+
 const app = express();
+
+app.use(cors());
 app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("hello world!");
 });
@@ -13,6 +18,11 @@ app.post("/decks", async (req: Request, res: Response) => {
   });
   const createdDeck = await newDeck.save();
   res.json(createdDeck);
+});
+
+app.get("/decks", async (req: Request, res: Response) => {
+  const decks = await Deck.find();
+  res.json(decks);
 });
 
 export default app;
